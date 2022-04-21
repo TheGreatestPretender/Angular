e1bd9a258b1392f750249343a200cf2e14fe069c
Kat Schmidt <siliconfoxx@protonmail.com>
2022-04-21 12:41:34 -0400

    WIP: creating custom pipes. Added angular defined pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name!: string;
+  date!: string;
+  amount!: number;
+  height!: number;
+  miles !: number;
+
+  car = {
+    make: 'Honda',
+    model: 'Civic Coupe',
+    year: 2019
+  }
+
+  //Event Handlers
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+}
